9bfd743520dd6928ee28fdb5dc881ffe5074d053
Sadik Saifi <Sadiksaifi205@gmail.com>
2023-08-14 06:00:31 +0530

    feat: initialize bread project

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import Button from './component/ui/Button'
+
+interface IHomeProps {}
+
+const Home: FC<IHomeProps> = ({}) => {
+  return (
+    <div>
+      <Button>Button</Button>
+    </div>
+  )
+}
+
+export default Home
